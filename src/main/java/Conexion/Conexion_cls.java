3375f26bd512d7a1dc0f8f6227f52bbde5270674
silva <silva@LAPTOP-BRIFAAR5>
2022-05-19 14:50:07 -0500

    Conexion establecida

--- src/main/java/Conexion/Conexion_cls.java
+++ src/main/java/Conexion/Conexion_cls.java
@@ -1,6 +1,29 @@
 package Conexion;
 
+import javax.sql.DataSource;
+
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+
 public class Conexion_cls {
+	private static BasicDataSource dataSource =null;
 	
+	private static DataSource getDataSource() {
+		if (dataSource==null) {
+			dataSource = new BasicDataSource();
+			dataSource.setDriverClassName("com.mysql.jc.jdbc.Driver");
+			dataSource.setUsername("root");
+			dataSource.setPassword("1914");
+			dataSource.setUrl("jdbc:mysql://localhost:3306/practica1");
+			dataSource.setInitialSize(20);
+			dataSource.setMaxIdle(15);
+			dataSource.setMaxTotal(20);
+			
+			
+			
+		}
+		
+		
+		return dataSource;
+	}
 
 }
