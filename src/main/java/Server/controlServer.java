1719facd471ee6453a000f459bc701caf7a6ba1d
silva <silva@LAPTOP-BRIFAAR5>
2022-05-19 14:06:34 -0500

    control index, conexion y modelo

---
+++ src/main/java/Server/controlServer.java
@@ -0,0 +1,36 @@
+package Server;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class controlServer extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public controlServer() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String opcion = request.getParameter("opcion");
+		if (opcion.equals("crear")) {
+			System.out.println("Presionado crear");
+		} else if(opcion.equals("listar")) {
+			System.out.println("Presionado listar");
+		}
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Usuarios netflix</title>
+<h1>Menu de operaciones</h1>
+</head>
+<body>
+<table border>
+<tr><td><a href="controlServer?opcion=crear">Insertar usuario</td></tr></a>
+<tr><td><a href="controlServer?opcion=listar">Listar usuarios</td></tr></a>
+
+</table>
+
+</body>
+</html>
